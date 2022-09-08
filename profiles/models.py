1d4691e8a14cc60e08e6fc46ac1eef176375ea4d
Rob Cole <rob-cole@hotmail.co.uk>
2022-09-08 18:48:52 +0000

    Create model for profiles app

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Model for User Profiles
+    """
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    screen_name = models.CharField(max_length=50)
+    full_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    created_date = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now_add=True)
+    profile_image = models.ImageField(
+        upload_to='images/', default='../default_profile_u9bikl'
+    )
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return f"{self.username}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(username=instance)
+
+
+post_save.connect(create_profile, sender=User)
