ede27ddb16725aa4302829d6461895cc726e145c
Rob Cole <rob-cole@hotmail.co.uk>
2022-09-21 17:39:20 +0000

    Add models to tutorial app

--- api_cadius/settings.py
+++ api_cadius/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'profiles',
     'servers',
     'ratings',
+    'tutorials',
 ]
 
 MIDDLEWARE = [
--- tutorials/models.py
+++ tutorials/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tutorial(models.Model):
+    """
+    """
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.TextField(max_length=50)
+    description = models.TextField(max_length=250)
+    video = models.FileField(upload_to='videos/', null=True, verbose_name='')
+    image = models.ImageField(upload_to='images/', blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
