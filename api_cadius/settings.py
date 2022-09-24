e4243ada11f7f98c74ce2bee4220b4e6e1d50164
Rob Cole <rob-cole@hotmail.co.uk>
2022-09-24 15:55:49 +0000

    Add models to comments app and update models on tutorials app

--- api_cadius/settings.py
+++ api_cadius/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'servers',
     'ratings',
     'tutorials',
+    'comments',
 ]
 
 MIDDLEWARE = [
--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tutorials.models import Tutorial
 
-# Create your models here.
+
+class comment(models.Model):
+    """
+    """
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tutorial = models.ForeignKey(Tutorial, on_delete=models.CASCADE)
+    description = models.CharField(max_length=250)
+    created_date = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ['-created_date']
