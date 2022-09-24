b245bb5b966cd3e7b9566284b96a0448c3828a83
Rob Cole <rob-cole@hotmail.co.uk>
2022-09-24 16:21:24 +0000

    Add class and fields to comments app serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,2 +1,18 @@
 from rest_framework import serializers
 from .models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.author
+    
+    class Meta:
+        model = Comment
+        fields = [
+            'id', 'tutorial', 'description', 'author', 'created_date',
+            'last_modified', 'is_owner' 
+        ]
