176df959fa0797cc0958116e859fadc969219afe
Rob Cole <rob-cole@hotmail.co.uk>
2022-09-24 16:46:20 +0000

    Remove obsolete imports from views

--- tutorials/views.py
+++ tutorials/views.py
@@ -1,7 +1,5 @@
-from django.http import Http404
-from rest_framework import generics, permissions, filters, status
+from rest_framework import generics, permissions
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from .models import Tutorial
 from .serializers import TutorialSerializer
 from api_cadius.permissions import IsOwner
