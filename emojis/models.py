6e79ab56c285f97340160dd20b43ac2adb01576f
Rob Cole <rob-cole@hotmail.co.uk>
2022-09-26 17:08:15 +0000

    Add models to Emjois App

--- emojis/models.py
+++ emojis/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tutorials.models import Tutorial
 
-# Create your models here.
+
+class Emoji(models.Model):
+    """
+    """
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tutorial = models.ForeignKey(Tutorial, on_delete=models.CASCADE)
+    emoji = models.TextField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ['-created_date']
