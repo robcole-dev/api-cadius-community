70de9c9c532efb49a8f5b528355499e194e8ca03
Rob Cole <rob-cole@hotmail.co.uk>
2022-10-18 22:11:47 +0000

    Modfiy Servers models to allow banner to be blank / null

--- servers/models.py
+++ servers/models.py
@@ -12,12 +12,15 @@ class Server(models.Model):
         ('mc', 'Minecraft')
     ]
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    game = models.CharField(max_length=40, choices=game_choices, default='pick')
+    game = models.CharField(
+        max_length=40, choices=game_choices, default='pick')
     server_name = models.TextField(max_length=50)
-    server_address = models.GenericIPAddressField(protocol='both', unpack_ipv4=True)
+    server_address = models.GenericIPAddressField(
+        protocol='both', unpack_ipv4=True)
     created_date = models.DateTimeField(auto_now_add=True)
     banner = models.ImageField(
-        upload_to='images/', default='../default_server_banner_hfurfy', blank=True
+        upload_to='images/', default='../default_server_banner_hfurfy',
+        blank=True, null=True
     )
 
     class Meta:
