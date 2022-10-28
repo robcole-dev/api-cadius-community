3bf4b6df00154c2c7640144ee202aa7b5f6b398f
Rob Cole <rob-cole@hotmail.co.uk>
2022-10-28 13:25:27 +0000

    Rollback changes on Profiles Views

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,45 @@
+from django.http import Http404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import generics
 from .models import Profile
 from .serializers import ProfileSerializer
 from api_cadius.permissions import IsOwner
 
 
-class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all().order_by('-created_date')
-    serializer_class = ProfileSerializer()
+class ProfileList(APIView):
+    def get(self, request):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(
+            profiles, many=True, context={'request': request})
+        return Response(serializer.data)
 
 
-class ProfileDetail(generics.RetrieveUpdateAPIView):
+class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
     permission_classes = [IsOwner]
-    queryset = Profile.objects.all().order_by('-created_date')
+
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, context={'request': request})
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data,
+                                       context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
