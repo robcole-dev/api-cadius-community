8dcd510d41a3aa41f4a092bfb239241a4c7e87de
Rob Cole <rob-cole@hotmail.co.uk>
2022-11-21 15:04:25 +0000

    Add client origin to settings

--- api_cadius/settings.py
+++ api_cadius/settings.py
@@ -53,6 +53,10 @@ DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost']
 
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get(
         'CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
