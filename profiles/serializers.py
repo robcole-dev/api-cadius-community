8f500f7d3dae7d88f2acfd7b21904aa389610622
Rob Cole <rob-cole@hotmail.co.uk>
2022-11-29 14:04:14 +0000

    Update Profiles serializer to include server_count

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,6 +5,7 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.ReadOnlyField(source='username.username')
     is_owner = serializers.SerializerMethodField()
+    server_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -14,5 +15,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'username', 'screen_name', 'full_name', 'last_name',
-            'created_date', 'last_modified', 'profile_image', 'is_owner'
+            'created_date', 'last_modified', 'profile_image', 'is_owner',
+            'server_count'
         ]
