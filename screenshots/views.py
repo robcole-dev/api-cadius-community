dd30d9e802b13897c1c357d68fd75736a520c7f0
Rob Cole <rob-cole@hotmail.co.uk>
2022-11-30 11:16:25 +0000

    Expand queryset for screenshots view

--- screenshots/views.py
+++ screenshots/views.py
@@ -8,7 +8,13 @@ from api_cadius.permissions import IsOwner
 class ScreenshotList(generics.ListCreateAPIView):
     serializer_class = ScreenshotSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Screenshot.objects.all()
+    queryset = Screenshot.objects.annotate().order_by('-created_date')
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'author_id__profile'
+    ]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -17,4 +23,4 @@ class ScreenshotList(generics.ListCreateAPIView):
 class ScreenshotDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwner]
     serializer_class = ScreenshotSerializer
-    queryset = Screenshot.objects.annotate()
+    queryset = Screenshot.objects.annotate().order_by('-created_date')
